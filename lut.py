62817a5c223259c68e27336e1c46892d255d4925
Bernhard Gstrein <bernhard-gstrein@mailbox.org>
2021-10-20 16:05:14 +0200

    Add optimized code to Python file

--- lut.py
+++ lut.py
@@ -1,168 +1,289 @@
 import numpy as np
 import copy
+import multiprocessing
+import os
+import pdb
 
-def count_recursive(cnt, x):
+def get_idxs(X, bit_pattern_tiled, N, bits):
     """
-    Given an array that stores counts of shape ([2] * (bits + 1)) (the last two dimensions
-    for y0 and y1, respectively), increase the count by 1 for given pattern (all but last
-    entries of x) and whether it's y0 or y1 (the last entry of x). This function is recursive.
+    Get indexes of bit pattern. Each row of X corresponds to one bit pattern, e.g.
+    `[0, 0, 1]`. The first entry of bit_pattern is `[0, 0, 0]`. So this function
+    would return index 1 for `[0, 0, 1]`.
 
     Parameters
     ==========
-    cnt: np.ndarray
-        Array that stores counts for a given dataset.
-    x: list
-        Single training example as list. The last entry is for y.
+    X: np.ndarray
+        Dataset of shape (N, bits) and dtype bool.
+    bit_pattern_tiled: np.ndarray
+        Tiled bit pattern: `np.tile(bit_pattern, (N, 1))`.
+    N: int
+        Number of examples, i.e. `X.shape[0]`.
+    bits:
+        Number of bits, i.e. `X.shape[1]`.
     """
-    if len(x) > 1:
-        idx = x.pop(0)
-        count_recursive(cnt[int(idx)], x)
-    else:
-        cnt[int(x[0])] += 1
+    return np.where(
+        np.all(bit_pattern_tiled == np.repeat(X, 2 ** bits, axis=0), axis=1,).reshape(
+            (N, 2 ** bits)
+        )
+        == True
+    )[1]
+
+def get_lut(indexes, labels, bits):
+    """
+    Get lookup table of length `2 ** bits` given an array that specifies which bit
+    pattern a training example belongs to. For example, if `x=[0, 0, 0]`, then the
+    respective entry in `indexes` is 0, if `x=[0, 1, 0]`, then it should be 2.
+
+    Parameters
+    ==========
+    indexes: array_like, int
+        Each entry corresponds to a row in the training set and specifies which
+        bit pattern it belongs to.
+    labels: array_like, int
+        Labels for the training set. Class 0 should be an int < 0, class 1 should be
+        an int > 0.
+    bits: int
+        Number of bits of the lut.
+    """
+    lut = np.bincount(indexes, weights=labels, minlength=2 ** bits)
+    where_rnd = (lut == 0)
+    np.put(lut, np.where(where_rnd)[0], np.random.choice([0, 1], size=(where_rnd).sum()))
+    np.put(lut, np.where(lut < 0)[0], 0)
+    np.put(lut, np.where(lut > 0)[0], 1)
+    return np.hstack((lut.astype(bool), where_rnd))
+
+def get_bit_pattern(bits):
+    """
+    See the example for explanation.
+
+    Parameters
+    ==========
+    bits: int
+
+    Returns
+    =======
+    np.ndarray
+
+    Examples
+    ========
+    >>> get_bit_pattern(3)
+    array([[False, False, False],
+           [False, False,  True],
+           [False,  True, False],
+           [False,  True,  True],
+           [ True, False, False],
+           [ True, False,  True],
+           [ True,  True, False],
+           [ True,  True,  True]])
+    """
+    bit_pattern = np.empty((2 ** bits, bits), dtype=bool)
+    for i in range(2 ** bits):
+        bit_string = np.binary_repr(i, width=bits)
+        for j, bit in enumerate(bit_string):
+            bit_pattern[i, j] = int(bit)
+    return bit_pattern
+
+def get_cols(inp_len, bits):
+    """
+    Get random assortment of columns for a lut to operate on.
+
+    Parameters
+    ==========
+    inp_len: int
+        Number of luts of previous layer.
+    bits: int
+        Bit size of the luts.
+
+    Returns
+    =======
+    np.ndarray
+    """
+    np.random.seed(int.from_bytes(os.urandom(4), byteorder="little"))
+    return np.random.choice(range(inp_len), size=bits)
 
 
 class Lut:
-    def __init__(self, bits):
-        self.bits = bits
-        self.lut = None
-        self.rnd = None
-        self.cnt = None
-        self.cols = None
-
-    def train(self, training_set, cols=None):
-        """
-        Train the lut given a training set.
-
-        Parameters
-        ==========
-        training_set: numpy.ndarray
-            Numpy array of shape (N, self.bits + 1) and dtype bool. The last column is for y.
-        cols: list, optional
-            List of indices to select columns. The lut will then be trained on a training set
-            with less columns.
-        """
-        assert self.lut is None, "Lut is already trained!"
-        if cols is not None:
-            assert len(cols) == self.bits, f"Number of selected columns has to match bit size"
-            self.cols = cols
-            cols_ = copy.deepcopy(cols)
-            cols_.append(-1)
-            training_set_ = training_set[:, cols_]
-        else:
-            self.cols = list(range(training_set.shape[1] - 1))
-            training_set_ = training_set
-
-        cnt = np.zeros([2] * (self.bits + 1), dtype=np.uint32)
-        for x in training_set_:
-            count_recursive(cnt, list(x))
-
-        cnt_ = cnt.reshape((-1, 2))
-
-        lut = np.zeros(([2] * (training_set_.shape[1] - 1)), dtype=bool)
-        rnd = np.zeros_like(lut)
-
-        for i in range(2 ** self.bits):
-            if cnt_[i, 0] > cnt_[i, 1]:
-                lut.ravel()[i] = 0
-                rnd.ravel()[i] = 0
-            elif cnt_[i, 0] < cnt_[i, 1]:
-                lut.ravel()[i] = 1
-                rnd.ravel()[i] = 0
-            else:
-                lut.ravel()[i] = np.random.choice([0, 1])
-                rnd.ravel()[i] = 1
-
-        self.lut = lut
-        self.cnt = cnt
-        self.rnd = rnd
-
-    def predict(self, data_set):
-        assert self.lut is not None, f"Lut not trained yet!"
-        preds = np.zeros((data_set.shape[0],), dtype=bool)
-        for idx, x in enumerate(data_set):
-            cols = copy.deepcopy(self.cols)
-            preds[idx] = classify_single_training_example(self, cols, x)
-        return preds
-
-    def __repr__(self):
-        if self.lut is not None:
-            return f"Trained lut, {self.bits} bits"
-        else:
-            return f"Empty lut, {self.bits} bits"
-
-    def get_lut_table(self):
-        if self.lut is not None:
-            string = ""
-            for i in range(2 ** self.bits):
-                bit_string = "0" * (self.bits - len(f"{i:b}")) + f"{i:b}"
-                is_rnd = "*" if self.rnd.ravel()[i] else ""
-                string += bit_string + "  " + f"{self.lut.ravel()[i]:d}" + is_rnd + "\n"
-            return string
-        else:
-            return f"Lut not trained yet, no table to return"
-
-    def __getitem__(self, idx):
-        return self.lut[idx]
-
-    def get_cnt_table(self):
-        """
-        Returns a string of the count table for the lut.
-        """
-        assert self.cnt is not None, "Lut not trained yet, no counts to return"
-        cnt_ = self.cnt.reshape((-1, 2))
-        string = ""
-        for i in range(2 ** self.bits):
-            bit_string = "0" * (self.bits - len(f"{i:b}")) + f"{i:b}"
-            string += bit_string + "  " + f"{cnt_[i, 0]} " + f"{cnt_[i, 1]}" + "\n"
-        return string
-
-
-def classify_single_training_example(lut, cols, training_example):
     """
-    Given a single training example and list of column indices that the lut operates on,
-    classify that bit pattern.
-
-    Parameters
-    lut: Lut
-        Lookup table object.
-    cols: list
-        List of columns that the lut operates on.
-    training_example: list
-        Single row from a training set.
-    """
-    if len(cols) > 1:
-        idx = cols.pop(0)
-        return classify_single_training_example(lut[int(training_example[idx])], cols, training_example)
-    else:
-        return lut[int(training_example[cols[0]])]
-
-
-def training_set_from_luts(luts, orig_training_set):
-    """
-    Given a list of luts trained on a subset of the original dataset, obtain a new training set
-    where the y labels are the same and the features come according to what the luts classify.
+    Create a single lut or a network of luts. If an empty list is passed for the
+    `hidden_layers` argument, then a single lut will be created.
 
     Parameters
     ==========
-    luts: array-like
-        List of luts.
-    orig_training_set: np.ndarray
-        Original training_set where the luts were trained on.
+    bits: int
+        Number of bits of each individual lut. When creating a single lut,
+        if the number of bits is the same as the number of features, then
+        all columns of the features will be used, otherwise a random assortment
+        of columns.
+    hidden_layers: list of ints
+        Hidden layer sizes. The depth of the lut network is determined by how many
+        ints are in this list. If empty, a single lut will be created.
+
+    Attributes
+    ==========
+    cols_arr_: list containing np.ndarrays
+        Each array corresponds to a layer and will have shape `(num_luts, bits)`
+        and dtype int. The entries specify which columns the luts operate on. For
+        example, `cols_arr_[0][1] = array([13, 5, 9])` meaning in the first hidden
+        layer, the 2nd lut takes columns 13, 5 and 9 from the training set.
+
+    lut_arr_: list containing np.ndarrays
+        Each array corresponds to a layer and will have shape `(num_luts, 2**bits)`.
+        A single lut is filled with boolean values. For example, if we have 3 bits
+        and `lut_arr_[1][2][0] = True`, that means that in the second hidden layer the
+        third lut classifies `[False, False, False]` with `True`.
+    rnd_arr_: list containing np.ndarrays
+        Each array corresponds to a layer and will have shape `(num_luts, 2**bits)`.
+        For each entry in `lut_arr_`, there is a corresponding entry in `rnd_arr_`.
+        If the corresponding entry in `rnd_arr_` is `True`, then the lut entry was
+        randomly selected during tie-breaking.
     """
-    training_set = np.zeros((orig_training_set.shape[0], len(luts) + 1), dtype=bool)
-    training_set[:, -1] = orig_training_set[:, -1]
-    for i in range(len(orig_training_set)):
-        for j in range(len(luts)):
-            cols = copy.deepcopy(luts[j].cols)
-            x = list(orig_training_set[i])
-            training_set[i, j] = classify_single_training_example(luts[j], cols, x)
+    def __init__(self, bits, hidden_layers=[]):
+        self.bits = bits
+        self.hidden_layers = hidden_layers
+        self.bit_pattern = get_bit_pattern(bits)
 
-    return training_set
+        self.cols_arr_ = []
+        self.lut_arr_ = []
+        self.rnd_arr_ = []
+
+    def train(self, X, y):
+        """
+        Train the network of luts or a single lut. The prediction for the training set
+        is returned because during training, propagation is already happening and this
+        way we don't have to call `predict()` and do it twice.
+
+        Parameters
+        ==========
+        X: np.ndarray
+            Training examples of shape `(N, bits)` and dtype bool.
+        y: np.ndarray
+            Labels of shape `(N,)` and dtype bool.
+
+        Returns
+        =======
+        preds_train: np.ndarray
+            Predictions for the training set.
+        """
+        assert X.dtype == bool, f"Dtype of X has to be bool, got {X.dtype}"
+        assert y.dtype == bool, f"Dtype of y has to be bool, got {y.dtype}"
+        N = X.shape[0]
+        bit_pattern_tiled = np.tile(self.bit_pattern, (N, 1))
+        y_ = y.copy().astype(int)
+        y_[y_ == 0] = -1
+        y_[y_ == 1] = 1
+
+        pool = multiprocessing.Pool()
+
+        if len(self.hidden_layers) > 0:
+            for j, num_luts in enumerate(self.hidden_layers):
+                self.cols_arr_.append(
+                    np.array(
+                        pool.starmap(
+                            get_cols,
+                            [
+                                [
+                                    X.shape[1] if j == 0 else self.hidden_layers[j - 1],
+                                    self.bits,
+                                ]
+                            ]
+                            * num_luts,
+                        )
+                    )
+                )
+                idxs = np.array(
+                    pool.starmap(
+                        get_idxs,
+                        [
+                            [
+                                X[:, self.cols_arr_[-1][i]]
+                                if j == 0
+                                else X_[:, self.cols_arr_[-1][i]],
+                                bit_pattern_tiled,
+                                N,
+                                self.bits,
+                            ]
+                            for i in range(num_luts)
+                        ],
+                    )
+                )
+                tmp = np.array(
+                        pool.starmap(
+                            get_lut,
+                            [[idxs[i], y_, self.bits] for i in range(num_luts)],
+                        )
+                    )
+                self.lut_arr_.append(tmp[:, :2**self.bits].copy())
+                self.rnd_arr_.append(tmp[:, 2**self.bits:].copy())
+                X_ = np.array([self.lut_arr_[-1][i][idxs[i]] for i in range(num_luts)]).T
+
+            self.cols_arr_.append(
+                np.random.choice(range(self.hidden_layers[-1]), size=self.bits)
+            )
+            idxs = get_idxs(
+                X_[:, self.cols_arr_[-1]], bit_pattern_tiled, N, self.bits
+            )
+            tmp = get_lut(idxs, y_, self.bits)
+            self.lut_arr_.append(tmp[:2**self.bits].copy())
+            self.rnd_arr_.append(tmp[2**self.bits:].copy())
+            preds_train = self.lut_arr_[-1][idxs]
+            return preds_train
+        else:
+            if X.shape[1] == self.bits:
+                self.cols_arr_.append(np.arange(X.shape[1]))
+            else:
+                self.cols_arr_.append(
+                    np.random.choice(X.shape[1], size=self.bits)
+                )
+            idxs = get_idxs(
+                X[:, self.cols_arr_[-1]], bit_pattern_tiled, N, self.bits
+            )
+            tmp = get_lut(idxs, y_, self.bits)
+            self.lut_arr_.append(tmp[:2**self.bits].copy())
+            self.rnd_arr_.append(tmp[2**self.bits:].copy())
+            preds_train = self.lut_arr_[-1][idxs]
+            return preds_train
 
 
-def get_lut_performance(lut, X_train, X_test):
-    preds = lut.predict(X_train)
-    print(f"Accuracy on training set: {accuracy_score(preds, X_train[:, -1]):.2f}%")
-    preds = lut.predict(X_test)
-    print(f"Accuracy on test set: {accuracy_score(preds, X_test[:, -1]):.2f}%")
-    print(f"{lut.rnd.sum() / len(lut.rnd.ravel()) * 100:.2f}% of lut entries are random")
+    def predict(self, X):
+        """
+        Predict using the lut classifier.
+
+        Parameters
+        ==========
+        X: np.ndarray
+            The input data of shape `(N, bits)` and dtype bool.
+
+        Returns
+        =======
+        preds: np.ndarray
+        """
+        assert X.dtype == bool, f"Dtype of X has to be bool, got {X.dtype}"
+        N = X.shape[0]
+        bit_pattern_tiled = np.tile(self.bit_pattern, (N, 1))
+
+        if len(self.hidden_layers) == 0:
+            X_ = X
+
+        pool = multiprocessing.Pool()
+        for j, num_luts in enumerate(self.hidden_layers):
+            idxs = np.array(
+                pool.starmap(
+                    get_idxs,
+                    [
+                        [
+                            X[:, self.cols_arr_[0][i]]
+                            if j == 0
+                            else X_[:, self.cols_arr_[j][i]],
+                            bit_pattern_tiled,
+                            N,
+                            self.bits,
+                        ]
+                        for i in range(num_luts)
+                    ],
+                )
+            )
+            X_ = np.array([self.lut_arr_[j][i][idxs[i]] for i in range(num_luts)]).T
+
+        idxs = get_idxs(X_[:, self.cols_arr_[-1]], bit_pattern_tiled, N, self.bits)
+        preds = self.lut_arr_[-1][idxs]
+        return preds
